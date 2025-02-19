51df56b4b267fd7709196e399813aceff0d38f0c
zahvafadhli <76609949+zahvafadhli@users.noreply.github.com>
2025-02-19 12:39:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Countdown Timer
+const countdown = () => {
+    const countDate = new Date("December 31, 2023 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
 
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day
